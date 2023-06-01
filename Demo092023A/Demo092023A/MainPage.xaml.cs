113ca069abca3df86aa74ce5ad857104fb3f07af
samuel-rome <samuel2004romero@gmail.com>
2023-05-31 23:16:45 -0500

    first commit

---
+++ Demo092023A/Demo092023A/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo092023A
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PanDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PinchDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SwipeDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TapDemo());
+            };
+        }
+    }
+}
---
+++ Demo092023A/Demo092023A/SwipeDemo.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo092023A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwipeDemo : ContentPage
+    {
+        public SwipeDemo()
+        {
+            InitializeComponent();
+        }
+        void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            string direction = e.Direction.ToString();
+
+            if (direction == "Right")
+                ((BoxView)sender).BackgroundColor = Color.Purple;
+            if (direction == "Left")
+                ((BoxView)sender).BackgroundColor = Color.Red;
+            if (direction == "Up")
+                ((BoxView)sender).BackgroundColor = Color.Black;
+            if (direction == "Down")
+                ((BoxView)sender).BackgroundColor = Color.Blue;
+
+            _label.Text = $"You siped: {e.Direction.ToString()}";
+        }
+    }
+}
